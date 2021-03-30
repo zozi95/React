160912b94fb643cf9850362f510b7f151f3b770e
kyun <zozi95@naver.com>
2021-03-30 13:30:46 +0900

    commit

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 //스타일
 import "./styles/app.scss";
 //컴포넌트 추가
 import Player from "./components/Player";
 import Song from "./components/Song";
+import data from "./util";
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
