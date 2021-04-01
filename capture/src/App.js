2396ca3aef082a32c0ac7d1d12c8b62f0cb58046
kyun <zozi95@naver.com>
2021-04-01 11:33:42 +0900

    2.About Section

--- capture/src/App.js
+++ capture/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import AboutUs from "./pages/AboutUs";
 function App() {
   return (
     <div className="App">
-      <h1>film</h1>
+      <AboutUs />
     </div>
   );
 }
