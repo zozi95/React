5263289f9d9c6ec2d8fbacf55099e4c9cce7c5de
kyun <zozi95@naver.com>
2021-04-02 14:02:29 +0900

    Our work animation

--- capture/src/animation.js
+++ capture/src/animation.js
@@ -7,7 +7,7 @@ export const pageAnimation = {
     opacity: 1,
     y: 0,
     transition: {
-      duration: 0.75,
+      duration: 0.5,
       when: "beforeChildren",
       staggerChildren: 0.25,
     },
@@ -45,4 +45,24 @@ export const photoAnim = {
       duration: 0.75,
     },
   },
+};
+export const lineAnim = {
+  hidden: { width: "0%" },
+  show: {
+    width: "100%",
+    transition: { duration: 1 },
+  },
+};
+
+export const slider = {
+  hidden: { x: "-130%", skew: "45deg" },
+  show: {
+    x: "100%",
+    skew: "0deg",
+    transition: { ease: "easeOut", duration: 1 },
+  },
+};
+export const sliderContainer = {
+  hidden: { opacity: 1 },
+  show: { opacity: 1, transition: { staggerChildren: 0.13, ease: "easeOut" } },
 };
