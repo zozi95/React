b2f5b8ccab6d26c83db4f8687760d72c0141dddd
kyun <zozi95@naver.com>
2021-04-02 10:24:17 +0900

    0402 1024

--- capture/src/App.js
+++ capture/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
-import Nav from "./components/Nav";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
+import Nav from "./components/Nav";
+import MovieDetail from "./pages/MovieDetail";
 
 import { Switch, Route } from "react-router-dom";
 function App() {
@@ -15,8 +16,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
         </Route>
         <Route path="/contact">
           <ContactUs />
