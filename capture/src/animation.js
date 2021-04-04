41ab5134f8540b86ced7e4ca9849c1eb3f957653
kyun <zozi95@naver.com>
2021-04-04 20:42:37 +0900

    Scroll

--- capture/src/animation.js
+++ capture/src/animation.js
@@ -66,3 +66,14 @@ export const sliderContainer = {
   hidden: { opacity: 1 },
   show: { opacity: 1, transition: { staggerChildren: 0.13, ease: "easeOut" } },
 };
+
+export const scrollReveal = {
+  hidden: { opacity: 0, scale: 0.8, transition: { duration: 0.5 } },
+  show: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.75,
+    },
+  },
+};
