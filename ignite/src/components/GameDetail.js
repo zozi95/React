fb2bfdb62fe60ebe9b53c78345d4523dd032cab3
kyun <zozi95@naver.com>
2021-04-08 16:17:01 +0900

    Rating

--- ignite/src/components/GameDetail.js
+++ ignite/src/components/GameDetail.js
@@ -10,6 +10,8 @@ import nintendo from "../img/nintendo.svg";
 import apple from "../img/apple.svg";
 import gamepad from "../img/gamepad.svg";
 import xbox from "../img/xbox.svg";
+import starEmpty from "../img/star-empty.png";
+import starFull from "../img/star-full.png";
 
 const GameDetail = ({ pathId }) => {
   const history = useHistory();
@@ -19,6 +21,19 @@ const GameDetail = ({ pathId }) => {
       document.body.style.overflow = "auto";
       history.push("/");
     }
+  };
+
+  const getStars = () => {
+    const stars = [];
+    const rating = Math.floor(game.rating);
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        stars.push(<img alt="star" key={i} src={starFull}></img>);
+      } else {
+        stars.push(<img alt="star" key={i} src={starEmpty}></img>);
+      }
+    }
+    return stars;
   };
 
   const getPlatform = (platform) => {
@@ -48,12 +63,14 @@ const GameDetail = ({ pathId }) => {
               <div className="rating">
                 <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
                 <p>Rating:{game.rating}</p>
+                {getStars()}
               </div>
               <Info>
                 <h3>Platforms </h3>
                 <Platforms>
                   {game.platforms.map((data) => (
                     <img
+                      alt={data.platform.name}
                       key={data.platform.id}
                       src={getPlatform(data.platform.name)}
                     ></img>
@@ -125,6 +142,11 @@ const Stats = styled(motion.div)`
   display: flex;
   align-items: center;
   justify-content: center;
+  img {
+    width: 2rem;
+    height: 2rem;
+    display: inline;
+  }
 `;
 
 const Info = styled(motion.div)`
